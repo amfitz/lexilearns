d7538a70c3f42e2519d9afbaf36d27bd2292ed0b
amfitz <lexilui@hotmail.com>
2016-06-18 15:31:08 -0700

    cerrected comment model

--- blog/models.py
+++ blog/models.py
@@ -15,6 +15,10 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
+
 class Comment(models.Model):
     post = models.ForeignKey('blog.Post', related_name='comments')
     author = models.CharField(max_length=200)
@@ -28,6 +32,3 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.text
-
-def approved_comments(self):
-    return self.comments.filter(approved_comment=True)
