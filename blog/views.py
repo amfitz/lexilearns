977faebbf0bf089315605083f058a505e2cb20bc
amfitz <lexilui@hotmail.com>
2016-06-21 19:50:43 -0700

    corrected post view order and fixed draft save/publish issue.

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.decorators import login_required
 
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 
@@ -35,7 +35,6 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk = post.pk)
     else:
